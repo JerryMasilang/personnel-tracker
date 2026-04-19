937d390cf3e15346f8661e80fd488510294513c2
JERRY MASILANG <masilang.jerry06@gmail.com>
2026-04-19 23:00:44 +0800

    Prepare settings for Render deployment

--- personnel_tracker/settings.py
+++ personnel_tracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vwvqivf2rs8!@p4a0+uv873m$60tkj1+j-1*44mvi$00sbf)+k'
 
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
