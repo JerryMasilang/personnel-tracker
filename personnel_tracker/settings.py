9c9842edcc8ff2adebe1dddb199a5035a90c3617
JERRY MASILANG <masilang.jerry06@gmail.com>
2026-04-20 08:04:00 +0800

    Configure WhiteNoise for static files in production

--- personnel_tracker/settings.py
+++ personnel_tracker/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
   
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'personnel_tracker.urls'
@@ -117,3 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
